b4a73253acbe3f076cb3062652ce8a845463c4b5
weberest <felipeestradawevar%40gmail.com>
2024-05-05 06:57:50 -0400

    Se agregó la confirmación de solicitud al cliente+admin

---
+++ js/formulario.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('button');
+
+    document.getElementById('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        btn.value = 'Enviando...';
+
+        const clientServiceID = 'default_service';
+        const clientTemplateID = 'template_92dt7vi';
+
+        // Envía correo electrónico al cliente 
+        emailjs.sendForm(clientServiceID, clientTemplateID, this)
+            .then(() => {
+                btn.value = 'Enviar';
+                alert('Mensaje enviado con éxito al cliente!');
+            })
+            .catch((err) => {
+                btn.value = 'Enviar';
+                alert("Hubo un error al enviar el mensaje al cliente. Por favor, inténtelo de nuevo más tarde.");
+                console.error("Error al enviar el correo electrónico al cliente:", err);
+            });
+
+        // Cambio de credenciales para utilizar cuenta de Felipe( ahi esta el template personalizado)
+        emailjs.init('lseOVV4yYvhVpXnfN');
+
+        const adminServiceID = 'service_nq9tgzu'; // Reemplaza con el Service ID del administrador
+        const adminTemplateID = 'template_g334a77'; // Reemplaza con el Template ID del administrador
+
+        // Envía correo electrónico al administrador
+        emailjs.sendForm(adminServiceID, adminTemplateID, this)
+            .then(() => {
+                console.log("Correo electrónico enviado con éxito al administrador");
+            })
+            .catch((err) => {
+                console.error("Error al enviar el correo electrónico al administrador:", err);
+            });
+    });
+});
